04f535ddb687ab41844861cb86052629071bc042
Knight Tan <knight.ashodin@gmail.com>
2014-05-13 22:20:56 +0800

    initial

---
+++ wp-snappy.php
@@ -0,0 +1,18 @@
+<?php 
+
+/*
+Plugin Name:       WP Snappy
+Plugin URI:        https://github.com/imknight/wp-snappy
+Description:       <a href="http://besnappy.com/">Snappy</a> WordPress Plugin
+Version:           0.0.1
+Author:            Knight
+License:           GNU General Public License v2
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+Domain Path:       /languages
+Text Domain:       wp-snappy
+GitHub Plugin URI: imknight/wp-snappy
+GitHub Branch:     master
+*/
+
+
+?>
